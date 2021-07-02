9f0369d168d5c5b99938ee7de02b2fa9f4f48ca3
ponnarasi <85145307+ponnarasik@users.noreply.github.com>
2021-07-02 01:56:32 +0000

    Initial commit

---
+++ launchCodeRocks.js
@@ -0,0 +1,20 @@
+function launchOutput(num){
+  let output ="";
+  if(num%2==0){
+    output += "Launch";
+    }
+    return output; 
+  else if(num%3==0){
+console.log("Code");
+    }else if(num%5==0){
+      console.log("Rocks");
+    }
+    if(num%2 !==0 &&
+    num%3 !==0 && num%5!==0)
+    {
+      console.log("Rutabagas! That doesnt work");//your code here*/
+    }
+
+
+
+module.exports = launchOutput;
---
+++ spec/launchCodeRocks.spec.js
@@ -0,0 +1,17 @@
+const launchOutput = require('../launchCodeRocks.js');
+
+
+describe("launchOutput", function(){
+it("returns 'Launch!' when passed a number divisible only by 2",
+function(){
+let output =  launchOutput(2);
+assert.strictEqual
+});
+expect(actual).toEqual(expected);
+})
+ /*it("number divisible by 3",
+  function(){
+    let expected = 'Code!'
+    let actual = launchOutput(3)
+    expect(actual).toEqual(expected);
+});
